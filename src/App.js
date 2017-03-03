6d761b8e36027bb47f6f4d58dd9934765c4bfc09
Antony Budianto <antonybudianto@gmail.com>
2017-03-03 22:44:50 +0700

    extract out firebase config to dotenv

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ import NotFoundPage from './ErrorPage/NotFoundPage/NotFoundPage';
 class App extends Component {
   componentWillMount() {
     // Initialize Firebase
-    let config = {
-      apiKey: "AIzaSyB4G1bUTLKRwTD1Qx88AnC1hfxaaGySQVk",
-      authDomain: "antonybudianto-web.firebaseapp.com",
-      databaseURL: "https://antonybudianto-web.firebaseio.com",
-      storageBucket: "antonybudianto-web.appspot.com",
-      messagingSenderId: "952722082016"
+    const config = {
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID
     };
     firebase.initializeApp(config);
   }
