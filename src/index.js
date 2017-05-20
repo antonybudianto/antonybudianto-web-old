7b5b031b1e2413c7994fc3fece47a3b402d6cf4e
Antony Budianto <antonybudianto@gmail.com>
2017-05-20 09:28:53 +0700

    update cra script 1.0.1 with PWA

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from 'react-snapshot';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 render(
   <App />,
   document.getElementById('root')
 );
+
+registerServiceWorker();
