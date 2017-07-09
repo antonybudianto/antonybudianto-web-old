ffc9daa450905755ed884fe6bb10ec009258ce36
Antony Budianto <antonybudianto@gmail.com>
2017-07-09 10:56:38 +0700

    implement ImageLoader to Portfolio images

--- src/PortfolioPage/Portfolio/Portfolio.js
+++ src/PortfolioPage/Portfolio/Portfolio.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import ImageLoader from '../../common/ImageLoader/ImageLoader';
 import './Portfolio.css';
 
 const Portfolio = ({portfolio}) =>
   <div className="col-md-4 no-padding portfolio">
     <div className="portfolio-image-wrapper">
-      <img
-      className="img-responsive img-thumbnail"
-      src={portfolio.imageUrl} alt="Coming soon"/>
+      <ImageLoader
+        className="img-responsive img-thumbnail"
+        src={portfolio.imageUrl} alt="Coming soon">
+        <i className="fa fa-spin fa-spinner fa-lg"></i>
+      </ImageLoader>
       <div className="portfolio-label">
         <a href={portfolio.url}>{portfolio.name}</a>
       </div>
