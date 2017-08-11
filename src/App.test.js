bc17a82c1decd0c26a98a2343ff78dbaee35c778
Antony Budianto <antonybudianto@gmail.com>
2017-08-11 20:42:06 +0700

    add unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import LandingPage from './LandingPage/LandingPage';
+import NotFoundPage from './ErrorPage/NotFoundPage/NotFoundPage';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+jest.mock('firebase/app');
+
+test('invalid path should redirect to 404', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={[ '/random' ]}>
+      <App/>
+    </MemoryRouter>
+  );
+  expect(wrapper.find(LandingPage)).toHaveLength(0);
+  expect(wrapper.find(NotFoundPage)).toHaveLength(1);
+});
+
+test('valid path should not redirect to 404', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={[ '/' ]}>
+      <App/>
+    </MemoryRouter>
+  );
+  expect(wrapper.find(LandingPage)).toHaveLength(1);
+  expect(wrapper.find(NotFoundPage)).toHaveLength(0);
 });
