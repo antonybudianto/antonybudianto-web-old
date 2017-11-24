148d39c9d356ca7f7b81496995e30a1603ac507a
Antony Budianto <antonybudianto@gmail.com>
2017-11-24 19:58:13 +0700

    refactor code and add gh star

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,42 +4,68 @@ import HomeSection from './HomeSection/HomeSection';
 import AppHeader from '../common/AppHeader';
 import './LandingPage.css';
 
+const githubRepos = [
+  'antonybudianto/cra-universal',
+  'antonybudianto/angular-starter',
+  'antonybudianto/express-firebase',
+  'antonybudianto/express-firebase-middleware'
+]
+
+const mediumBlogs = [
+  {
+    title: 'Server-side rendering with React and Firebase Functions',
+    url: 'https://medium.com/@antonybudianto/server-side-rendering-with-react-and-firebase-functions-cd67fdb2b605'
+  },
+  {
+    title: 'Testing React Router apps with Jest and Enzyme',
+    url: 'https://medium.com/@antonybudianto/react-router-testing-with-jest-and-enzyme-17294fefd303'
+  },
+  {
+    title: 'Automate your create-react-app build on Heroku using Buildpack',
+    url: 'https://medium.com/@antonybudianto/automate-your-create-react-app-build-on-heroku-using-buildpack-beb77408ffee'
+  }
+]
+
 const ListSection = () => (
-    <div className="LandingPage-section List-container">
-        <div className="mobile-flex" style={{
-            display: 'flex',
-            width: '100%',
-            justifyContent: 'space-evenly'
-        }}>
-            <div className="List-section">
-                <h4><i className="fa fa-github"></i> Latest repos</h4>
-                <ul className="list-style-none">
-                    <li><a href="https://github.com/antonybudianto/cra-universal">Create React App Universal CLI</a></li>
-                    <li><a href="https://github.com/antonybudianto/express-firebase-middleware">Express Firebase Middleware</a></li>
-                    <li><a href="https://github.com/antonybudianto/express-firebase">Express Firebase Starter</a></li>
-                </ul>
-            </div>
-            <div className="List-section">
-                <h4><i className="fa fa-medium"></i> Latest blogs</h4>
-                <ul className="list-style-none">
-                    <li>
-                        <a href="https://medium.com/@antonybudianto/server-side-rendering-with-react-and-firebase-functions-cd67fdb2b605">
-                            Server-side rendering with React and Firebase Functions</a></li>
-                    <li><a href="https://medium.com/@antonybudianto/react-router-testing-with-jest-and-enzyme-17294fefd303">
-                            Testing React Router apps with Jest and Enzyme</a></li>
-                    <li><a href="https://medium.com/@antonybudianto/automate-your-create-react-app-build-on-heroku-using-buildpack-beb77408ffee">
-                            Automate your create-react-app build on Heroku using Buildpack</a></li>
-                </ul>
-            </div>
+  <div className="LandingPage-section List-container">
+    <div className="mobile-flex" style={{
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-evenly'
+    }}>
+        <div className="List-section">
+            <h4><i className="fa fa-github"></i> Latest repos</h4>
+            <ul className="list-style-none">
+              {
+                githubRepos.map((gh, i) => (
+                  <li key={i}><a href={`https://github.com/${gh}`}>{gh}</a> &nbsp;
+                    <img src={`https://img.shields.io/github/stars/${gh}.svg`} alt={gh}/>
+                  </li>
+                ))
+              }
+            </ul>
+        </div>
+        <div className="List-section">
+          <h4><i className="fa fa-medium"></i> Latest blogs</h4>
+          <ul className="list-style-none">
+            {
+              mediumBlogs.map((md, i) => (
+                <li key={i}>
+                  <a href={md.url}>{md.title}</a>
+                </li>
+              ))
+            }
+          </ul>
         </div>
     </div>
+  </div>
 )
 
 const LandingPage = () =>
-    <div>
-        <AppHeader></AppHeader>
-        <HomeSection></HomeSection>
-        <ListSection />
-    </div>
+  <div>
+    <AppHeader></AppHeader>
+    <HomeSection></HomeSection>
+    <ListSection />
+  </div>
 
 export default LandingPage;
