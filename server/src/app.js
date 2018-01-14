fb80ba83d734f3335732dd439bdfd22e378e90b8
Antony Budianto <antonybudianto@gmail.com>
2018-01-14 20:32:06 +0700

    finish upgrade cra-express and handle 404

--- server/src/app.js
+++ server/src/app.js
@@ -4,13 +4,15 @@ const path = require('path');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const { StaticRouter } = require('react-router');
-const { createReactAppExpress } = require('create-react-app-express');
+const { createReactAppExpress } = require('@cra-express/core');
+const stringRenderer = require('@cra-express/universal-loader/lib/renderer/string-renderer').default;
 
 const {default: App} = require('../../src/App');
 const clientBuildPath = path.resolve(__dirname, 'client');
 let tag = ''
 const app = createReactAppExpress({
   clientBuildPath,
+  handleRender: stringRenderer,
   universalRender: handleUniversalRender,
   onEndReplace(html) {
     return html.replace('<div id="script"></div>', `${tag}`)
@@ -26,7 +28,16 @@ function handleUniversalRender(req, res) {
   )
   return getLoadableState(app).then(loadableState => {
     tag = loadableState.getScriptTag();
-    return ReactDOMServer.renderToNodeStream(app);
+    const str = ReactDOMServer.renderToString(app);
+
+    if (context.status === 404) {
+      return res.status(404).send(str);
+    }
+    if (context.url) {
+      return res.redirect(301, context.url);
+    }
+
+    return str;
   });
 }
 
