34d88a0b818e51177557d841800dac08d2c13709
Antony Budianto <antonybudianto@gmail.com>
2018-07-01 14:58:26 +0700

    update dep

--- crau.config.js
+++ crau.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    modifyWebpack: function (config) {
-        var newConfig = Object.assign({}, config,
-            {output: Object.assign({}, config.output,
-                {libraryTarget: 'commonjs2'}),
-            entry: './server/app.js'});
-        return newConfig;
-    }
+  modifyWebpack: function(config) {
+    var newConfig = Object.assign({}, config, {
+      output: Object.assign({}, config.output, { libraryTarget: 'commonjs2' }),
+      entry: './server/index.js'
+    });
+    return newConfig;
+  }
 };
