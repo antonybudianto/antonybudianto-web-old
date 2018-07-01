9cf677ae9c4ca4d52cc501e5ff628dd87c7b573b
Antony Budianto <antonybudianto@gmail.com>
2018-07-01 13:32:08 +0700

    use microbundle

--- crau.config.js
+++ crau.config.js
@@ -1,15 +1,9 @@
-'use strict';
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 module.exports = {
-  modifyWebpack: function modifyWebpack(config) {
-    var newConfig = _extends({}, config, {
-      output: _extends({}, config.output, {
-        libraryTarget: 'commonjs2'
-      }),
-      entry: './server/app.js'
-    });
-    return newConfig;
-  }
+    modifyWebpack: function (config) {
+        var newConfig = Object.assign({}, config,
+            {output: Object.assign({}, config.output,
+                {libraryTarget: 'commonjs2'}),
+            entry: './server/app.js'});
+        return newConfig;
+    }
 };
