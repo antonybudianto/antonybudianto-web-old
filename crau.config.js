ac81bf8d209ee64e3e0885174027bbf862f1a545
Antony Budianto <antonybudianto@gmail.com>
2019-09-05 23:57:59 +0700

    update deps and node 10 cloud function

--- crau.config.js
+++ crau.config.js
@@ -1,4 +1,5 @@
 const env = process.env.NODE_ENV || 'development';
+
 module.exports = {
   modifyWebpack: config => {
     const newConfig = {
@@ -7,7 +8,7 @@ module.exports = {
         ...config.output,
         libraryTarget: 'commonjs2'
       },
-      entry: env ? './server/index.js' : './server/app.js'
+      entry: env === 'development' ? './server/index.js' : './server/app.js'
     };
     return newConfig;
   }
