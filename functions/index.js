9c3d6887d02144399fe6ee1c8c1ad96acf29db96
Antony Budianto <antonybudianto@gmail.com>
2019-09-06 00:40:18 +0700

    update dep (#14)

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
 
-const app = require('./crau-dist/server/bundle');
+const app = require('./crau-dist/server/bundle').default;
 
 exports.app = functions.https.onRequest(app);
